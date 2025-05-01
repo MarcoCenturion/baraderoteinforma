dad3657e634c87e390fbdfc16fb70da943b88530
marcocenturion <aulavirtualturismo@gmail.com>
2025-05-01 20:38:12 -0300

    Novedad

--- baraderoteinforma/baraderoteinforma/settings.py
+++ baraderoteinforma/baraderoteinforma/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-tjky4p9hi+^0d+ejm2=2_$$%1nwn9ivo+=@c8u^74mouezjdd$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
